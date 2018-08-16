9fb4032d67fbec542042dd24df7961bd1334eff9
Justin Toh <tohjustin@hotmail.com>
2018-08-15 23:04:25 -0700

    refactor(pkg/badge): Remove transform/scale hack

--- pkg/badge/badge.go
+++ pkg/badge/badge.go
@@ -142,9 +142,6 @@ func GenerateSVG(style, subject, status, color string) (string, error) {
 
 	badgeSVGTemplate := packr.NewBox("./assets/badge-templates").String(newBadge.TemplateFilename)
 	t := template.New(newBadge.TemplateFilename)
-	t.Funcs(template.FuncMap{
-		"multiply": func(a, b int) int { return a * b },
-	})
 	t.Parse(badgeSVGTemplate)
 
 	var buf bytes.Buffer
