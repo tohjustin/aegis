d89df565ab1da3eeed787316f4a801479422f0ac
Justin Toh <tohjustin@hotmail.com>
2018-08-19 20:11:38 -0700

    feat(badgeService): Add Cache-Control response headers

--- badgeService.go
+++ badgeService.go
@@ -22,6 +22,8 @@ func badgeServiceHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	// cache response in browser for 1 day (86400), CDN for 1 year (31536000)
+	w.Header().Set("Cache-Control", "public, immutable, max-age=86400, s-maxage=31536000")
 	w.Header().Set("Content-Type", "image/svg+xml;utf-8")
 	w.Write([]byte(generatedBadge))
 }
