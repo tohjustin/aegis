23d3e45e4437d2f5ad50dc7faf1239199e742815
Justin Toh <tohjustin@hotmail.com>
2018-08-28 22:27:36 -0700

    feat(main): Modify server to shutdown gracefully

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"time"
 
 	"github.com/gorilla/mux"
 	"github.com/urfave/negroni"
@@ -32,5 +35,32 @@ func main() {
 	n.Use(newRecoveryMiddleware())
 	n.UseHandler(newRouter())
 
-	http.ListenAndServe(":"+port, n)
+	srv := http.Server{
+		Addr:         ":" + port,
+		Handler:      n,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
+	log.Printf("HTTP service listening on port %s...", port)
+
+	// gracefully shutdowns server
+	idleConnsClosed := make(chan struct{})
+	go func() {
+		sigint := make(chan os.Signal, 1)
+		signal.Notify(sigint, os.Interrupt)
+		<-sigint
+
+		if err := srv.Shutdown(nil); err != nil {
+			log.Printf("HTTP service Shutdown: %v", err)
+		}
+
+		log.Printf("HTTP service shutdown successfully...")
+		close(idleConnsClosed)
+	}()
+
+	if err := srv.ListenAndServe(); err != http.ErrServerClosed {
+		log.Printf("HTTP service ListenAndServe: %v", err)
+	}
+
+	<-idleConnsClosed
 }
